4f39dad7c9828df180fffd7ff5bbcfc4bb955bf1
flynn-zhou <19557114131@163.com>
2024-09-24 00:48:54 +0800

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect, useContext } from "react";
 import { ethers } from "ethers";
 
 import { UniswapV3DemoContext } from "../context/UniswapV3DemoContext";
+import { CREATE_POOL_INFO } from "../context/constants";
 
 const index = () => {
   const { currentAccount, checkIfWalletConnected, connectWallet, createPool } =
     useContext(UniswapV3DemoContext);
 
-  const [poolAddress, setPoolAddress] = useState("Pool Not Create");
+  const [poolAddress, setPoolAddress] = useState(
+    "Pool Not Create, click to create..."
+  );
   const [chainId, setChainId] = useState(0);
 
   const initialPool = async () => {
@@ -19,6 +22,8 @@ const index = () => {
     setPoolAddress(poolAdd);
   };
 
+  const currentCreatedPools = CREATE_POOL_INFO.POOL_ADDRESS;
+
   // useEffect(() => {
   //   connectWallet();
   // }, []);
@@ -26,9 +31,7 @@ const index = () => {
   return (
     <div style={{ textAlign: "center" }}>
       <br />
-      -----------------------分割----------------------
-      <br />
-      <button onClick={() => connectWallet()}>Connect Wallet</button>
+      <button onClick={() => connectWallet()}>Step 1: Connect Wallet</button>
       <br />
       -----------------------分割----------------------
       <br />
@@ -38,15 +41,43 @@ const index = () => {
       <br />
       -----------------------分割----------------------
       <br />
-      <button onClick={() => initialPool()}>Create Pool</button>
+      <h3>
+        New pool address： <span>{poolAddress}</span>
+      </h3>
+      <button onClick={() => initialPool()}>
+        &nbsp; &nbsp;Step 2: Create Pool&nbsp; &nbsp;{" "}
+      </button>
       <br />
-      -----------------------分割----------------------
       <br />
-      <h2>New pool address： {poolAddress}</h2>
+      <ul>
+        <br />
+        <button>
+          Step 3: Paste [New pool address] to constants.js.POOL_ADDRESS
+        </button>
+
+        <br />
+        <br />
+        <li>
+          <h2> Created Pool list</h2>
+        </li>
+        <br />
+        {currentCreatedPools.map((pool, index) => (
+          <li key={index + 1}>
+            <span>{pool}</span>
+          </li>
+        ))}
+      </ul>
+      <br />
+      <br />
+      <br />
+      <h3>添加本地网络</h3>
       <br />
       <button onClick={() => checkIfWalletConnected()}>
         Add [local_fork_sepolia] to MetaMask
       </button>
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
